7790d88db06da4f8e2a2157fb619615f2cc68ad1
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-04-26 03:43:55 +0200

    Finish Day 50

--- code/00-starting-project/app.js
+++ code/00-starting-project/app.js
@@ -8,47 +8,37 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
 app.get('/restaurants', (req, res) => {
-  res.render('restaurants');
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const data = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(data);
+    res.render('restaurants', {restaurants: storedRestaurants});
 });
-
-app.get('/recommend', (req, res) => {
-  res.render('recommend');
-});
-
-app.get('/confirm', (req, res) => {
-  res.render('confirm');
-});
-
+app.get('/confirm', (req, res) => res.render('confirm'));
+app.get('/recommend', (req, res) => res.render('recommend'));
 app.post('/recommend', (req, res) => {
-  const restaurant = req.body;
+    const restaurant = req.body;
 
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
-  const data = fs.readFileSync(filePath);
-  const restaurantsData = JSON.parse(data);
-  restaurantsData.push(restaurant);
+    const data = fs.readFileSync(filePath);
+    const restaurantsData = JSON.parse(data);
+    restaurantsData.push(restaurant);
 
-  fs.writeFile(filePath, JSON.stringify(restaurantsData), (err) => {
-    if (err) {
-      return res.statusCode(500).send('Internal Server Error');
-    } else {
-      res.redirect('/confirm');
-    }
-  });
+    fs.writeFile(filePath, JSON.stringify(restaurantsData), (err) => {
+        if (err) {
+            return res.statusCode(500).send('Internal Server Error');
+        } else {
+            res.redirect('/confirm');
+        }
+    });
 });
 
 app.listen(port, () => {
-  console.log('Server listening on port ' + port);
+    console.log('Server listening on port ' + port);
 });
